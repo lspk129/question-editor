2a12be374c8ba83931aab6dea820415cd02be9c5
Linas <l.spukas@gmail.com>
2017-08-31 17:22:23 +0300

    Setup project, add eslint with airbnb style guide

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = { count: 0 };
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default App;
